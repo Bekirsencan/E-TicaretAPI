ad5adb916d2e63f1ff439cb820fd21bed0d9cdc0
Bekir <PmyoViki@gmail.com>
2021-09-27 20:21:50 +0300

    context_processors created & code refactor.

--- store/models.py
+++ store/models.py
@@ -1,17 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return super(ProductManager, self).get_queryset().filter(is_active=True)
 
 
 class Category(models.Model):
     name = models.CharField(max_length=255, db_index=True)
     slug = models.SlugField(max_length=255, unique=True)
-    objects = models.Manager()
 
     class Meta:
         verbose_name_plural = 'categories'
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('store:category_list', args=[self.slug])
 
 
 class Product(models.Model):
@@ -27,9 +35,15 @@ class Product(models.Model):
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-
     objects = models.Manager()
+    products = ProductManager()
 
     class Meta:
         verbose_name_plural = 'products'
         ordering = ('-created',)
+
+    def get_absolute_url(self):
+        return reverse('store:product_detail', args=[self.slug])
+
+    def __str__(self):
+        return self.title
